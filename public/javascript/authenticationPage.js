ede59735a8c0a44d4b2db6a7dbc77a858d3990d9
SineRiseX <bfu01.bb@gmail.com>
2024-04-20 02:31:13 +0200

    update

--- public/javascript/authenticationPage.js
+++ public/javascript/authenticationPage.js
@@ -98,7 +98,7 @@ document.addEventListener("submit", async function(e) {
 			hasSubmitted = false;
 		} else {
 			clearInterval(loadingCredentials);
-			responseText.style.color = "yellow";
+			responseText.style.color = "red";
 			responseText.textContent = `Invalid credentials (${data.error})`;
 			hasSubmitted = false;
 		} 
